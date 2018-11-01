6c6f8e2a69fd7f2dfa20eacaccbda3d34ed19439
Vkk26 <vkk26@njit.edu>
2018-11-01 19:01:33 -0400

    UnitTest for inserting a user into users table

---
+++ tests/Unit/InsertUserTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Tests\Unit;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+class InsertUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testInsertUser()
+    {
+        $user = factory(\App\User::class)->make();
+        $this->assertDatabaseHas('users', ["name" => 'Carol Nitzsche I']);
+    }
+}
