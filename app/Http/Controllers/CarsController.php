6a2bf5db627a78f03151c2374156939ad3a8e011
Vkk26 <vkk26@njit.edu>
2018-11-01 13:52:01 -0400

    Created model cars and added 3 properties Make,Model,Year

---
+++ app/Http/Controllers/CarsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\cars;
+use Illuminate\Http\Request;
+
+class CarsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\cars  $cars
+     * @return \Illuminate\Http\Response
+     */
+    public function show(cars $cars)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\cars  $cars
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(cars $cars)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\cars  $cars
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, cars $cars)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\cars  $cars
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(cars $cars)
+    {
+        //
+    }
+}
---
+++ app/cars.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class cars extends Model
+{
+    protected $fillable = [
+        'Make', 'Model', 'year',
+    ];
+}
